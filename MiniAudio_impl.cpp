63199e0d09dc89c381fac3c871f9f32cbf02bc2f
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-04-17 12:23:41 +0200

    split out miniaudio implementation into separate file

---
+++ MiniAudio_impl.cpp
@@ -0,0 +1,7 @@
+
+// Separate file to cut down on compile times
+#define MINIAUDIO_IMPLEMENTATION
+#define MA_NO_JACK
+#define MA_NO_SDL
+#define MA_NO_OPENAL
+#include <miniaudio/miniaudio.h>
--- Modem.cpp
+++ Modem.cpp
@@ -1,14 +1,13 @@
 #include "Modem.h"
+
 #include <QDebug>
 
 #include <cmath>
 
-#define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_JACK
 #define MA_NO_SDL
 #define MA_NO_OPENAL
 #include <miniaudio/miniaudio.h>
-
 
 #define BAUDS 300
 
