b2df9b6cad37f3328e3437d6f615cf7540d030fa
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-04-22 21:14:01 +0200

    higher samplerate is better sample rate

--- Modem.cpp
+++ Modem.cpp
@@ -11,8 +11,7 @@
 
 #define DEVICE_FORMAT       ma_format_f32
 #define DEVICE_CHANNELS     1
-#define DEVICE_SAMPLE_RATE 41000
-//#define DEVICE_SAMPLE_RATE  48000
+#define DEVICE_SAMPLE_RATE 48000
 
 Modem::Modem(QObject *parent) : QObject(parent)
 {
